00944c869d0e349b9f60ab219844c765fc825b29
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2022-02-20 17:20:02 +0000

    #9 Tided up, added logging, save data, main loop.
    Signed-off-by: Christopher Menon <16004217+cmenon12@users.noreply.github.com>

--- motion_detection.py
+++ motion_detection.py
@@ -22,29 +22,21 @@ __license__ = "gpl-3.0"
 # The name of the config file
 CONFIG_FILENAME = "config.ini"
 
-# How long to wait for authorization (in seconds)
-AUTHORIZATION_TIMEOUT = 300
-
-# File with the OAuth client secret
-CLIENT_SECRET_FILE = "client_secret.json"
-
-# API-specific credentials
-SCOPES = ["https://www.googleapis.com/auth/youtube"]
-
-# File with the user's access and refresh tokens
-TOKEN_PICKLE_FILE = "token.pickle"
+# File with the list of completed IDs
+SAVE_DATA_FILENAME = "save_data.json"
 
 # The timezone to use throughout
 TIMEZONE = timezone("Europe/London")
 
 
 def get_complete_broadcasts(service: googleapiclient.discovery.Resource) -> Dict[str, List[str]]:
-    # Get all the broadcasts
-    LOGGER.debug("Fetching all the broadcasts...")
+    """Get a list of all complete broadcast IDs."""
+
+    LOGGER.info("Fetching the complete broadcasts...")
     next_page_token = ""
     all_broadcasts = []
     while next_page_token is not None:
-        response = yt_livestream.YouTubeLivestream.execute_request(service.playlists().list(
+        response = yt_livestream.YouTubeLivestream.execute_request(service.liveBroadcasts().list(
             part="id,status",
             mine=True,
             maxResults=50,
@@ -57,15 +49,20 @@ def get_complete_broadcasts(service: googleapiclient.discovery.Resource) -> Dict
 
     # Collate the complete broadcasts
     complete_broadcasts = []
-    for item in response["items"]:
+    for item in all_broadcasts:
         if item["status"]["lifeCycleStatus"] == "complete":
             complete_broadcasts.append(item["id"])
-    LOGGER.debug("new_broadcasts is: \n%s.", json.dumps(complete_broadcasts, indent=4))
+    LOGGER.debug("complete_broadcasts is: %s.", complete_broadcasts)
 
+    LOGGER.info("Complete broadcasts fetched successfully!")
     return complete_broadcasts
 
 
-def download_video(video_id: str):
+def download_video(video_id: str) -> str:
+    """Download the YouTube video and return the filename."""
+
+    LOGGER.info("Downloading video...")
+    LOGGER.info(locals())
     ydl_opts = {
         "logger": LOGGER,
         "format": "best[height=144]+[ext=mp4]",
@@ -73,17 +70,20 @@ def download_video(video_id: str):
         "final_ext": "mp4"
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        print("here")
         ydl.download(f"https://youtu.be/{video_id}")
         filename = ydl.extract_info(
             f"https://youtu.be/{video_id}")["requested_downloads"][0]["_filename"]
+        LOGGER.debug("Video filename is %s.", filename)
 
+    LOGGER.info("Video downloaded successfully!")
     return filename
 
 
 def get_motion_timestamps(filename: str):
     """Detect motion and output a list of when it occurs."""
 
+    LOGGER.info("Detecting motion...")
+    LOGGER.info(locals())
     output = ""
     scan = dvr_scan.scanner.ScanContext([filename])
     scan.set_event_params(
@@ -94,6 +94,9 @@ def get_motion_timestamps(filename: str):
                        FrameTimecode]] = scan.scan_motion()
     for item in result:
         output += f"{item[0].get_timecode(0)} for {humanize.naturaldelta(item[2].get_seconds())}.\n"
+    LOGGER.debug("Output is: \n%s.", output)
+
+    LOGGER.info("Motion detected successfully!")
     return output
 
 
@@ -117,9 +120,44 @@ def main():
     main_config = parser["yt_livestream"]
     email_config = parser["email"]
 
-    filename = download_video("eIT3lHx4jq0")
-    motion_detected = get_motion_timestamps(filename)
-    print(motion_detected)
+    # Get access to the YouTube API
+    service = yt_livestream.YouTubeLivestream.get_service()
+
+    # Try and load the list of completed IDs
+    try:
+        with open(SAVE_DATA_FILENAME, "r") as file:
+            complete_broadcasts = json.load(file)
+        LOGGER.info("Loaded save data %s", complete_broadcasts)
+    except FileNotFoundError:
+        LOGGER.info("Could not find save data %s, using empty list.", SAVE_DATA_FILENAME)
+        complete_broadcasts = []
+
+    while True:
+
+        # Find out which videos need processing
+        new_ids = []
+        new_complete_broadcasts = get_complete_broadcasts(service)
+        for video_id in new_complete_broadcasts:
+            if video_id not in complete_broadcasts:
+                new_ids.append(video_id)
+                complete_broadcasts.append(video_id)
+        LOGGER.debug("new_ids is: %s.", new_ids)
+
+        # Process them
+        for video_id in new_ids:
+            print(f"Processing {video_id}...")
+            filename = download_video(video_id)
+            motion_detected = get_motion_timestamps(filename)
+            print(motion_detected)
+
+        # Save the new completed IDs
+        if len(new_ids) != 0:
+            with open(SAVE_DATA_FILENAME, "w") as file:
+                json.dump(complete_broadcasts, file)
+                LOGGER.debug("Saved the save data to %s successfully.", SAVE_DATA_FILENAME)
+
+        # Wait before repeating
+        time.sleep(15 * 60)
 
 
 if __name__ == "__main__":
