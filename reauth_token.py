5c6d5d57278debb828392162bebdfe7bc67c06dc
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2022-03-08 20:22:59 +0000

    Updated reauth_token.py to work with the latest changes.
    Signed-off-by: Christopher Menon <16004217+cmenon12@users.noreply.github.com>

--- reauth_token.py
+++ reauth_token.py
@@ -1,21 +1,41 @@
 """Reauthorise and replace the Google API token."""
-
+import configparser
 import os
 
 import yt_livestream
 
+__author__ = "Christopher Menon"
+__credits__ = "Christopher Menon"
+__license__ = "gpl-3.0"
+
+# The token filenames to use
 NEW_TOKEN_PICKLE_FILE = "new-token.pickle"
 OLD_TOKEN_PICKLE_FILE = yt_livestream.TOKEN_PICKLE_FILE
+
+# The name of the config file
+CONFIG_FILENAME = "config.ini"
 
 
 def main():
     """Reauthorise and replace the token."""
 
+    # Check that the config file exists
+    try:
+        open(CONFIG_FILENAME)
+    except FileNotFoundError as error:
+        print("The config file doesn't exist!")
+        raise FileNotFoundError("The config file doesn't exist!") from error
+
+    # Fetch info from the config
+    parser = configparser.ConfigParser()
+    parser.read(CONFIG_FILENAME)
+    yt_config = parser["YouTube"]
+
     # Reauthorise to a new file
     if os.path.exists(NEW_TOKEN_PICKLE_FILE):
         os.remove(NEW_TOKEN_PICKLE_FILE)
-    service = yt_livestream.YouTubeLivestream.get_service(
-        token_file=NEW_TOKEN_PICKLE_FILE)
+    yt = yt_livestream.YouTube(yt_config)
+    yt.get_service(auth_type=yt_livestream.AuthorizationTypes.SSH, token_file=NEW_TOKEN_PICKLE_FILE)
 
     # Replace the old file
     if os.path.exists(OLD_TOKEN_PICKLE_FILE):
