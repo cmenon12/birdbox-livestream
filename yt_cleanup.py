305aca8c9f5168f317a5ba2f39224524cdfd7f6b
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2024-01-20 16:25:58 +0000

    #56 Added support for updating or deleting playlists

--- yt_cleanup.py
+++ yt_cleanup.py
@@ -1,7 +1,9 @@
 import configparser
+import json
 import logging
 import sys
 from datetime import datetime
+from typing import List
 
 from pick import pick
 from pytz import timezone
@@ -12,6 +14,7 @@ __license__ = "gpl-3.0"
 
 import utilities
 from google_services import YouTube
+from yt_types import YouTubePlaylist
 
 # The name of the config file
 CONFIG_FILENAME = "config.ini"
@@ -23,6 +26,65 @@ TIMEZONE = timezone("Europe/London")
 LOG_FILENAME = f"birdbox-livestream-yt-livestream-{datetime.now(tz=TIMEZONE).strftime('%Y-%m-%d %H-%M-%S %Z')}.txt"
 
 
+def update_weekly_playlists(yt: YouTube, start_date: datetime = None, end_date: datetime = None,
+                            delete: bool = False, privacy: str = "private", ):
+    LOGGER.info("Updating playlists...")
+    LOGGER.info(locals())
+
+    playlists: List[YouTubePlaylist] = []
+    next_page_token = ""
+
+    while True:
+        response = yt.execute_request(yt.get_service().playlists().list(
+            part="id,snippet",
+            mine=True,
+            maxResults=50,
+            pageToken=next_page_token
+        ))
+
+        for playlist in response["items"]:
+            date = datetime.strptime(playlist["snippet"]["publishedAt"], "%Y-%m-%dT%H:%M:%SZ")
+            if (start_date and date < start_date) or (end_date and date > end_date):
+                continue
+            if ": w/c" in playlist["snippet"]["title"]:
+                playlists.append(playlist)
+        LOGGER.debug("Found %s new playlists.", len(response["items"]))
+
+        try:
+            next_page_token = response["nextPageToken"]
+        except KeyError:
+            break
+
+    LOGGER.debug("Found %s playlists in total.", len(playlists))
+
+    titles = [playlist["snippet"]["title"] for playlist in playlists]
+    print(
+        f"Are you sure you want to {'delete' if delete else privacy} all these playlists?\n{json.dumps(titles, indent=4)}")
+    answer = input("y/n: ")
+    LOGGER.info("User chose option %s.", answer)
+
+    if answer != "y":
+        LOGGER.info("User chose not to continue, exiting.")
+        sys.exit()
+
+    for playlist in playlists:
+
+        if delete:
+            r = yt.execute_request(yt.get_service().playlists().delete(
+                id=playlist["id"]
+            ))
+            LOGGER.debug("Deleted playlist %s %s.", playlist["id"], playlist["snippet"]["title"])
+        else:
+            body = {"id": playlist["id"], "status": {"privacyStatus": "private"}}
+            r = yt.execute_request(yt.get_service().playlist().update(
+                part="id,status",
+                body=body
+            ))
+            LOGGER.debug("Updated playlist %s %s.", playlist["id"], playlist["snippet"]["title"])
+
+    LOGGER.info("Updated the playlists successfully!")
+
+
 def main():
     # Get the config
     parser = utilities.load_config(CONFIG_FILENAME)
@@ -32,13 +94,19 @@ def main():
 
     title = "What do you want to do?"
     options = ["make weekly playlists private", "delete weekly playlists"]
-    option, index = pick(options, title)
+    option, _ = pick(options, title)
     LOGGER.info("User chose option %s.", option)
 
+    start_date = input("Enter the start date (YYYY-MM-DD): ")
+    start_date = datetime.strptime(start_date, "%Y-%m-%d") if start_date else None
+
+    end_date = input("Enter the end date (YYYY-MM-DD): ")
+    end_date = datetime.strptime(end_date, "%Y-%m-%d") if end_date else None
+
     if option == "make weekly playlists private":
-        update_playlists(yt, delete=False, privacy="private")
+        update_weekly_playlists(yt, start_date, end_date, delete=False, privacy="private")
     elif option == "delete weekly playlists":
-        update_playlists(yt, delete=True)
+        update_weekly_playlists(yt, start_date, end_date, delete=True, )
 
 
 if __name__ == "__main__":
